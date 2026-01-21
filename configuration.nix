92fa36e01194987cac65d92bf1f2039392f5478e
victorytek <victorytech@proton.me>
2026-01-21 09:00:22 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,18 +1,42 @@
+{ config, pkgs, ... }:
+
 {
-  description = "NixOS configuration with flakes for nimda";
+  # NixOS configuration for vex-nix
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  # Import modules
+  imports = [
+    ./modules/packages/packages.nix
+    ./modules/desktop/gnome.nix
+  ];
+
+  # Bootloader configuration
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "vex-nix";
+  networking.networkmanager.enable = true;
+
+  # Time zone and localization
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system
+  services.xserver.enable = true;
+
+  # User account
+  users.users.nimda = {
+    isNormalUser = true;
+    description = "nimda";
+    extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.vex-nix = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-      modules = [
-        ./configuration.nix
-        /etc/nixos/hardware-configuration.nix
-      ];
-    };
-  };
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # System state version
+  system.stateVersion = "24.11";
 }
