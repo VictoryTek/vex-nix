2f2237a958e72a2c248381797c25b5ff5d7e6b67
victorytek <victorytech@proton.me>
2026-01-22 10:33:46 -0600

    Update system.nix

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -35,69 +35,85 @@ in
   # Enable dconf
   programs.dconf.enable = true;
 
-  # Install gschema override file for GNOME defaults
-  environment.etc."dconf/db/local.d/01-vex".text = ''
-    # GDM Login Logo
-    [org/gnome/login-screen]
-    logo='/usr/share/pixmaps/vex.png'
+  # Configure dconf database for GNOME system-wide defaults
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        # GDM Login Logo
+        "org/gnome/login-screen" = {
+          logo = "/usr/share/pixmaps/vex.png";
+        };
 
-    # Desktop Interface Settings
-    [org/gnome/desktop/interface]
-    color-scheme='prefer-dark'
-    accent-color='teal'
-    icon-theme='kora'
-    cursor-theme='Bibata-Modern-Classic'
-    cursor-size=24
+        # Desktop Interface Settings
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          accent-color = "teal";
+          icon-theme = "kora";
+          cursor-theme = "Bibata-Modern-Classic";
+          cursor-size = 24;
+        };
 
-    # Desktop Background
-    [org/gnome/desktop/background]
-    picture-uri='file:///etc/wallpapers/vex-bb-light.jxl'
-    picture-uri-dark='file:///etc/wallpapers/vex-bb-dark.jxl'
-    picture-options='zoom'
+        # Desktop Background
+        "org/gnome/desktop/background" = {
+          picture-uri = "file:///etc/wallpapers/vex-bb-light.jxl";
+          picture-uri-dark = "file:///etc/wallpapers/vex-bb-dark.jxl";
+          picture-options = "zoom";
+        };
 
-    # Screensaver Settings
-    [org/gnome/desktop/screensaver]
-    picture-uri='file:///etc/wallpapers/vex-bb-dark.jxl'
-    picture-options='zoom'
-    lock-enabled=false
+        # Screensaver Settings
+        "org/gnome/desktop/screensaver" = {
+          picture-uri = "file:///etc/wallpapers/vex-bb-dark.jxl";
+          picture-options = "zoom";
+          lock-enabled = false;
+        };
 
-    # Power Settings
-    [org/gnome/settings-daemon/plugins/power]
-    sleep-inactive-ac-type='nothing'
-    sleep-inactive-battery-type='nothing'
+        # Power Settings
+        "org/gnome/settings-daemon/plugins/power" = {
+          sleep-inactive-ac-type = "nothing";
+          sleep-inactive-battery-type = "nothing";
+        };
 
-    # Window Manager Preferences
-    [org/gnome/desktop/wm/preferences]
-    button-layout='appmenu:minimize,maximize,close'
+        # Window Manager Preferences
+        "org/gnome/desktop/wm/preferences" = {
+          button-layout = "appmenu:minimize,maximize,close";
+        };
 
-    # Shell Settings
-    [org/gnome/shell]
-    disable-user-extensions=false
-    enabled-extensions=${enabledExtsText}
-    favorite-apps=['com.brave.Browser.desktop', 'io.gitlab.librewolf-community.desktop', 'tv.plex.PlexDesktop.desktop', 'io.freetubeapp.FreeTube.desktop', 'org.gnome.Nautilus.desktop', 'com.mitchellh.ghostty.desktop', 'system-update.desktop']
+        # Shell Settings
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = enabledList;
+          favorite-apps = [
+            "com.brave.Browser.desktop"
+            "io.gitlab.librewolf-community.desktop"
+            "tv.plex.PlexDesktop.desktop"
+            "io.freetubeapp.FreeTube.desktop"
+            "org.gnome.Nautilus.desktop"
+            "com.mitchellh.ghostty.desktop"
+            "system-update.desktop"
+          ];
+        };
 
-    # Logo Widget Extension
-    [org/gnome/shell/extensions/logo-widget]
-    logo-file='/usr/share/pixmaps/vex.png'
-    logo-file-dark='/usr/share/pixmaps/vex.png'
+        # Logo Widget Extension
+        "org/gnome/shell/extensions/logo-widget" = {
+          logo-file = "/usr/share/pixmaps/vex.png";
+          logo-file-dark = "/usr/share/pixmaps/vex.png";
+        };
 
-    # Dash to Dock Extension
-    [org/gnome/shell/extensions/dash-to-dock]
-    dock-position='LEFT'
+        # Dash to Dock Extension
+        "org/gnome/shell/extensions/dash-to-dock" = {
+          dock-position = "LEFT";
+        };
 
-    # Touchpad Settings
-    [org/gnome/desktop/peripherals/touchpad]
-    tap-to-click=true
+        # Touchpad Settings
+        "org/gnome/desktop/peripherals/touchpad" = {
+          tap-to-click = true;
+        };
 
-    # Privacy Settings
-    [org/gnome/desktop/privacy]
-    remember-recent-files=true
-  '';
-
-  # Update dconf database after changes
-  system.activationScripts.dconf-update = lib.mkAfter ''
-    if [ -x "$(command -v dconf)" ]; then
-      ${pkgs.dconf}/bin/dconf update
-    fi
-  '';
+        # Privacy Settings
+        "org/gnome/desktop/privacy" = {
+          remember-recent-files = true;
+        };
+      };
+    }
+  ];
 }
