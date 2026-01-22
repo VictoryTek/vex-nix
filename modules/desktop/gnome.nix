83461291752d703d0e4c842aab00efaa74ae3fd5
victorytek <victorytech@proton.me>
2026-01-22 12:36:03 -0600

    Update gnome.nix

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -3,11 +3,11 @@
 {
   # Enable GNOME Desktop Environment with GDM
   services.xserver.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Enable Wayland on GDM (default)
-  services.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.wayland = true;
 
   # Configure keymap
   services.xserver.xkb = {
