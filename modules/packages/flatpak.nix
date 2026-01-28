9d55376d8b3216dd773c450eb8df6d464e86510c
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2026-01-27 20:34:52 -0600

    Add Flatpak app installation to activation script

--- modules/packages/flatpak.nix
+++ modules/packages/flatpak.nix
@@ -10,4 +10,14 @@
   system.activationScripts.flatpak-repo = ''
     ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
   '';
+  
+  # Install Flatpak packages on activation
+  # Add Flatpak app IDs here (find them on flathub.org)
+  system.activationScripts.flatpak-install = ''
+    # Example: ${pkgs.flatpak}/bin/flatpak install -y flathub com.spotify.Client
+    
+    flatpak install -y flathub com.brave.Browser
+    flatpak install -y flathub app.zen_browser.zen
+    
+  '';
 }
