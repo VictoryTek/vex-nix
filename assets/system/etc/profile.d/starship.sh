0b8d4bb7971bb2da6e981cebdca1a6fff2882b1e
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2026-03-08 10:21:14 -0500

    Delete assets directory

--- assets/system/etc/profile.d/starship.sh
+++
@@ -1,4 +0,0 @@
-# Set global Starship config for all shells unless user already set one
-if [ -z "${STARSHIP_CONFIG}" ] && [ -f /etc/starship.toml ]; then
-  export STARSHIP_CONFIG=/etc/starship.toml
-fi
